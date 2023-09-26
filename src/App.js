20a46c560ed79aabb45e76cee22da17b2df87174
Jeff H <cuk8@txstate.edu>
2023-09-25 20:58:38 -0500

    add simple textbox

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
+import TextInput from "./components/TextInput";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <TextInput />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/TextInput.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+function TextInput() {
+  const [text, setText] = useState("");
+
+  const handleChange = (event) => {
+    setText(event.target.value);
+  };
+
+  return (
+    <div>
+      <input type="text" value={text} onChange={handleChange} />
+    </div>
+  );
+}
+
+export default TextInput;
