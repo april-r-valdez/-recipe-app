8b89db238e0c2892ba2f5ea259bd4b7de73b286a
Brent Hoover <bah4@txstate.edu>
2023-09-26 23:49:58 -0500

    Add useState hook that sets ingredients from textbox

--- src/Input.js
+++ src/Input.js
@@ -1,15 +1,28 @@
-
+import { useState } from "react";
 
 const  Textbox = () => {
+    const [ingredient, setIngredient] = useState('');
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const input = {ingredient};
+        console.log(input);
+    };
+
     return (  
         <div className="textbox">
             <p>Enter your ingredients here</p>
-            <input
-            placeholder='Enter ingredients'
-            ></input>
-            <div>
-                <button>Generate Recipes</button>
-            </div>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type='text'
+                    placeholder='Enter ingredients'
+                    onChange={(e) => setIngredient(e.target.value)}
+                ></input>
+                <div>
+                    <button>Generate Recipes</button>
+                </div>
+                <p>{ingredient}</p>
+            </form>
         </div>
     );
 }
