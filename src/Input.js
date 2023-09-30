de7d00fa8e662593b9e25227f099dc3d52be50a2
Brent Hoover <bah4@txstate.edu>
2023-09-30 00:18:59 -0500

    Restructure and fix handler functions

--- src/Input.js
+++ src/Input.js
@@ -1,40 +1,91 @@
-import { useState, useEffect } from "react";
-import { v4 as uuidv4 } from 'uuid'; // generates unique id for map() function
+import { useState, useEffect } from 'react'
 
 const  Textbox = () => {
-    // useState hook updates the variables ingredient and listIngredients
-    const [ingredient, setIngredient] = useState('');
-    const [listIngredients, setIngredientsList] = useState([]);
+    // useState hook updates variables storing user inputs
+    const [inputs, setInputs] = useState({
+        // default values set
+        ingredient:'',
+        amount:'',
+        units:''
+    });
 
-    // handleSubmit function called when typing in input field
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const newIngredient = {id: uuidv4(), item:ingredient}
-        setIngredientsList([...listIngredients, newIngredient])
-    };
+    // useState hook updates list of ingredients
+    const [ingredientsList, setIngredientsList] = useState([]);
+
+    // adds object containing user inputs to ingredients list
+    const addIngredientsToList = () => {
+        setIngredientsList([...ingredientsList, inputs]);
+    }
+
+    // handleChange receives event from each input when form is submitted
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs(values => ({...values, [name]: value}))
+    }
+
+    // handleSubmit resets variables to default values
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        addIngredientsToList();
+        setInputs({ ingredient:'', amount:'', units:''}); 
+    }
 
     // useEffect hook runs function with every render of component
-    // [listIngredients] is a dependency added and will run function if it changes 
+    // [ingredientsList] is a dependency added and runs function if it changes 
     useEffect(() => {
-        console.log(listIngredients);
-    }, [listIngredients]); 
+    }, [ingredientsList]); 
 
     return (  
         <div className="textbox">
-            <p>Enter your ingredients here</p>
+            <br></br><p>Enter your ingredients here</p><br></br>
             <form onSubmit={handleSubmit}>
-                <input
-                    type='text'
-                    placeholder='Enter ingredients'
-                    onChange={(e) => setIngredient(e.target.value)}
-                ></input>
+                <div>
+                    <label>Ingredient:</label>
+                    <input
+                        required
+                        type='text'
+                        id='ingredient'
+                        name='ingredient'
+                        value={inputs.ingredient}
+                        placeholder='Ex: eggs, milk, butter...'
+                        onChange={handleChange}
+                    ></input>
+                </div><br></br>
+                <div>
+                    <label>Amount:</label>
+                    <input
+                        required
+                        type='number'
+                        min='1'
+                        id='amount'
+                        name='amount'
+                        value={inputs.amount}
+                        placeholder='Ex: 1 egg, 2 cups,...'
+                        onChange={handleChange}
+                    ></input> 
+                </div><br></br>
+                <div>
+                    <label>Units:</label> 
+                    <select id='units' name='units' value={inputs.units} onChange={handleChange}>
+                        <option value=''></option>
+                        <option value='tsp'>tsp</option>
+                        <option value="tbsp">tbsp</option>
+                        <option value="lbs">lbs</option>
+                        <option value='fluid oz'>fluid oz</option>
+                        <option value="cups">cups</option>
+                        <option value="pints">pints</option>
+                        <option value="quarts">quarts</option>
+                        <option value="gallons">gallons</option>
+                    </select>   
+                </div><br></br>
+                <input name='form' type='submit' value='Generate Recipe'></input>
             </form>
-            <button>Add Ingredients</button>
-            <h2>Your Pantry:</h2>
-            {listIngredients.map((entry) => (
-                <div key={entry.id}>
-                    <p>{entry.item}</p>
-                </div>
+            <br></br><br></br><h2>Your Pantry:</h2><br></br>
+            {ingredientsList.map((input, index) => (
+            <ul key={index}>
+              {input.amount} {input.units} of {input.ingredient}
+            </ul>
             ))}
         </div>
     );
