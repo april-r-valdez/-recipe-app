e3969cdfc499f9928485c5bbf82ba19ae2a6f9d6
Brent Hoover <bah4@txstate.edu>
2023-10-03 18:16:56 -0500

    Rearrange and add more unit tests

--- src/components/__tests__/UserInput.test.js
+++ src/components/__tests__/UserInput.test.js
@@ -10,27 +10,34 @@ afterEach(() => {
     cleanup();
 });
 
-
 describe("UserInput", () => {
     test('test1', () => {
         render(<UserInput/>);
         const textbox = screen.getByRole("textbox");
-        expect(textbox).toBeInTheDocument();
-    });
-    
-    test('test2', () => {
-        render(<UserInput/>);
         const spinButton = screen.getByRole("spinbutton");
-        expect(spinButton).toBeInTheDocument();
-    });
-    
-    test('test3', () => {
-        render(<UserInput/>);
         const button = screen.getByRole("button");
+        expect(textbox).toBeInTheDocument();
+        expect(spinButton).toBeInTheDocument();
         expect(button).toBeInTheDocument();
     });
-})
 
+    test('test2', () => {
+        render(<UserInput/>);
+        render(<Ingredient inputs={[]} handleChange={() => {}}/>);
+        render(<Amount inputs={[]} handleChange={() => {}}/>);
+        render(<Units inputs={[]} handleChange={() => {}}/>);
+        const ingredientElement = screen.getByLabelText('Ingredient:');
+        const amountElement = screen.getByLabelText('Amount:');
+        const unitsElement = screen.getByLabelText('Units:');
+        const addButton = screen.getByText("Add ingredient");
+
+        fireEvent.change(ingredientElement, { target: { value: 'Test Ingredient' } });
+        fireEvent.change(amountElement, { target: { value: 'Test Amount' } });
+        fireEvent.change(unitsElement, { target: { value: 'Test Units' } });
+        fireEvent.click(addButton);
+
+    });
+})
 
 describe("Ingredient", () => {
     test('should render ingredient element', () => {
@@ -70,18 +77,19 @@ describe("Amount", () => {
         expect(amountElement).toBeInTheDocument();
         expect(amountElement).toHaveAttribute('type', 'number');
         expect(amountElement).toHaveAttribute('name', 'amount');
+        expect(amountElement).toHaveAttribute('min', '1');
         expect(amountElement).toHaveAttribute('required');
 
         const placeholderText = screen.getByPlaceholderText("Ex: 1 egg, 2 cups,...");
         expect(placeholderText).toBeInTheDocument();
     });
 
-    // test('should be able to type in input', () => {
-    //     render(<Ingredient inputs={[]} handleChange={() => {}}/>);
-    //     const amountElement = screen.getByPlaceholderText("Ex: 1 egg, 2 cups,...");
-    //     fireEvent.change(amountElement, { target: {value: "5"} });
-    //     expect(amountElement.value).toBe("5");
-    // });
+    test('should be able to type amount', () => {
+        render(<Amount inputs={[]} handleChange={() => {}}/>);
+        const amountElement = screen.getByPlaceholderText("Ex: 1 egg, 2 cups,...");
+        fireEvent.change(amountElement, { target: {value: "5"} });
+        expect(amountElement.value).toBe("5");
+    });
 })
 
 describe("Units", () => {
@@ -91,12 +99,27 @@ describe("Units", () => {
         expect(unitsElement).toBeInTheDocument();
         expect(unitsElement).toHaveAttribute('name', 'units');
     });
+
+    test('should be able to select units', () => {
+        render(<Units inputs={[]} handleChange={() => {}}/>);
+        const unitsElement = screen.getByLabelText('Units:');
+        fireEvent.change(unitsElement, { target: {value: "cups"} });
+        expect(unitsElement.value).toBe("cups");
+
+    });
 })
 
 describe("Pantry", () => {
+
+    const ingredientsList = [
+        {ingredient: 'Flour', amount: 2, units: 'cups'},
+        // {ingredient: 'Butter', amount: 4, units: 'tbsp'}
+    ];
+
     test('should render pantry element', () => {
-        render(<Pantry ingredientsList={[]}/>);
-        expect(true).toBe(true);
+        const {getByText} = render(<Pantry ingredientsList={[]}/>);
+        const headingElement = getByText("Your Pantry:");
+        expect(headingElement).toBeInTheDocument();
     });
 })
 
