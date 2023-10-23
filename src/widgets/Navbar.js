5d7f26bc5b3f530a8a2d763fb4274fd41f202031
Brent Hoover <bah4@txstate.edu>
2023-10-23 18:39:23 -0500

    create sidebar component

--- src/widgets/Navbar.js
+++ src/widgets/Navbar.js
@@ -1,4 +1,5 @@
 import DisplayToggle from "../components/Utils/DisplayMode";
+import Sidebar from "../widgets/Sidebar";
 
 function Navbar() {
     return (
@@ -10,34 +11,8 @@ function Navbar() {
                     </button>
                     <a className="navbar-brand fw-bolder px-2" href="/"> RECIPE GENERATOR</a>
                 </div>
-                <div className="offcanvas offcanvas-start" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-                    <div className="offcanvas-header">
-                        <h5 className="offcanvas-title" id="offcanvasNavbarLabel">NAVIGATION</h5>
-                        <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                    </div>
-                    <div className="offcanvas-body">
-                        <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="/">Dashboard</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/">Link</a>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Dropdown
-                            </a>
-                            <ul className="dropdown-menu">
-                            <li><a className="dropdown-item" href="/">Action</a></li>
-                            <li><a className="dropdown-item" href="/">Another action</a></li>
-                            <li>
-                                <hr className="dropdown-divider/"/>
-                            </li>
-                            <li><a className="dropdown-item" href="/">Something else here</a></li>
-                            </ul>
-                        </li>
-                        </ul>
-                    </div>
+                <div> 
+                    <Sidebar/>
                 </div>
                 <div classname="d-flex">
                     <div className="row justify-content-end align-items-center">
