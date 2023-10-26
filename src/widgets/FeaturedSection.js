d788efb9352732175d1d15a20afc1bd6d82fc72b
Viet Vu <llv43@txstate.edu>
2023-10-26 09:04:45 -0500

    made rest api call to populate the feature section with random recipes

--- src/widgets/FeaturedSection.js
+++ src/widgets/FeaturedSection.js
@@ -1,20 +1,37 @@
+import { useEffect, useState } from "react";
 import PlaceholderCard from "../components/Common/PlaceholderCard";
 
+const API_KEY = '8218ef888f3545f08036a30afc6577d7';
 
 function FeaturedSection() {
+    const [recipes, setRecipes] = useState([]);
+
+    const getFeatureRecipes = async () => {
+        try {
+            const response = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${API_KEY}&number=9`);
+            if (!response.ok) {
+                throw new Error("Error fetching the data");
+            }
+            const data = await response.json();
+            setRecipes(data.recipes);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        getFeatureRecipes();
+    }, []);
+
     return (
         <div className="container-fluid">
             <h5>Featured Recipe</h5>
             <div className="row row-cols-1 row-cols-md-3 g-4">
-            <div className="col"><PlaceholderCard/></div>
-            <div className="col"><PlaceholderCard/></div>
-            <div className="col"><PlaceholderCard/></div>
-            <div className="col"><PlaceholderCard/></div>
-            <div className="col"><PlaceholderCard/></div>
-            <div className="col"><PlaceholderCard/></div>
-            <div className="col"><PlaceholderCard/></div>
-            <div className="col"><PlaceholderCard/></div>
-            <div className="col"><PlaceholderCard/></div>
+            {recipes.map((recipe) => (
+                <div key={recipe.id}>
+                    <PlaceholderCard recipe={recipe} />
+                </div>
+            ))}
             </div>
         </div>
     )
