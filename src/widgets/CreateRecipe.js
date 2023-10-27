366383afd59c074ba0f0dcbbd1b7a8b772ce50ac
Brent Hoover <bah4@txstate.edu>
2023-10-27 18:22:44 -0500

    add directions to list

--- src/widgets/CreateRecipe.js
+++ src/widgets/CreateRecipe.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import Navbar from "./Navbar";
 import Ingredient from '../components/Common/Ingredient';
 import Amount from '../components/Common/Amount';
@@ -7,7 +7,7 @@ import Direction from '../components/Common/Direction';
 import Pantry from '../components/Pantry/Pantry';
 
 const  UserInput = () => {
-    // useState hook updates variables storing user inputs
+    // useState hook updates variables that store user inputs
     const [inputs, setInputs] = useState({
         // default values set
         ingredient:'',
@@ -23,11 +23,14 @@ const  UserInput = () => {
         setIngredientsList([...ingredientsList, inputs]);
     }
 
-    // handleChange receives event from each input when form is submitted
+    // handleChange receives event from input and updates textbox
     const handleChange = (event) => {
         const name = event.target.name;
         const value = event.target.value;
-        setInputs(values => ({...values, [name]: value}))
+        if (name == "ingredient" || name == "amount" || name == "units")
+            setInputs(values => ({...values, [name]: value}))
+        else if (name == "direction")
+            setDirections(values => ({...values, [name]: value}))
     }
 
     // handleSubmit resets variables to default values
@@ -37,17 +40,29 @@ const  UserInput = () => {
         setInputs({ ingredient:'', amount:'', units:''}); 
     }
 
-    // useEffect hook runs function with every render of component
-    // [ingredientsList] is a dependency added and runs function if it changes 
-    useEffect(() => {
-    }, [ingredientsList]); 
+    
+    const [directions, setDirections] = useState({
+        direction:''
+    });
+
+    const [directionsList, setDirectionsList] = useState([]);
+
+    const addDirectionsToList = () => {
+        setDirectionsList([...directionsList, directions]);
+    }
+
+    const handleNewDirection = (event) => {
+        event.preventDefault();
+        addDirectionsToList();
+        setDirections({ direction: ''});
+    } 
 
     return (  
         <div className="container-xl">
             <div className="row">
                 <Navbar/>
             </div>
-            <br></br><h1 className="display-5">Create My Recipe</h1><br></br>
+            <br></br><h1 className="display-5">Create Recipe</h1><br></br>
             <div className="row">
                 <div className="col-lg-4">
                     <p className="h4">Add Ingredients</p><br></br>
@@ -60,17 +75,28 @@ const  UserInput = () => {
                             </div>
                         </form><br></br>
                     <br></br><p className="h4">Add Directions</p><br></br>
-                        <form onSubmit={handleSubmit}>
-                            <Direction /><br></br>
+                        <form onSubmit={handleNewDirection}>
+                            <Direction directions={directions} handleChange={handleChange}/><br></br>
                             <div class="d-grid gap-2 col-8 mx-auto">
                                 <button type="submit" class="btn btn-primary">Add Direction</button>
                             </div>
                         </form><br></br>
                 </div>
                 <div className="col-lg-8">
-                <p className="h4">My Recipe</p><br></br>
-                <Pantry ingredientsList={ingredientsList}/>
+                    <p className="h4">New Recipe</p><br></br>
+                    <Pantry ingredientsList={ingredientsList}/>
+
+                    <div className="pantry">
+                        {directionsList.map((directions, index) => (
+                        <ul key={index}>
+                            Step {index + 1}: <br></br>
+                            {directions.direction}
+                        </ul>
+                        ))}
+                    </div>
                 </div>
+
+                
             </div>
         </div>
     );
