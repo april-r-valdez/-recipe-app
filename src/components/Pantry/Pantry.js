876b390a019b0173b6f1b14d98a46b969595a253
Brent Hoover <bah4@txstate.edu>
2023-10-27 19:31:02 -0500

    add comments and updated Pantry

--- src/components/Pantry/Pantry.js
+++ src/components/Pantry/Pantry.js
@@ -1,13 +1,22 @@
 
-const Pantry = ({ingredientsList}) => {
+const Pantry = ({ingredientsList, directionsList}) => {
     return (
         <div className="pantry">
+            <p className="h5">Ingredients</p><br></br>
             {ingredientsList.map((input, index) => (
-            <ul key={index}>
-              {input.amount} {input.units} {input.ingredient}
-            </ul>
+                <ul key={index}>
+                {input.amount} {input.units} {input.ingredient}
+                </ul>
+            ))}
+            <p className="h5">Directions</p><br></br>
+            {directionsList.map((directions, index) => (
+                <ul key={index}>
+                    Step {index + 1}: <br></br>
+                    {directions.direction}
+                </ul>
             ))}
         </div>
+
     );
 }
 
