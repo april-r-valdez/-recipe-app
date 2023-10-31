c9c4d8e7a90f93c5c4df62ac90f1b6a7da637f44
Brent Hoover <bah4@txstate.edu>
2023-10-30 19:27:40 -0500

    change display of created recipe

--- src/components/Pantry/Pantry.js
+++ src/components/Pantry/Pantry.js
@@ -17,25 +17,30 @@ const Pantry = ({recipeInfo, ingredientsList, directionsList}) => {
                     </div><br></br><br></br>
                 </div>
             )}
-            <p className="h5 text-start">Ingredients</p><br></br>
-            {ingredientsList.map((input, index) => (
-                <ul key={index}>
-                    <div className="row">
-                        <div className="list-group-item d-flex justify-content-between align-items-start">
-                            {input.amount} {input.units} {input.ingredient}
-                        </div><br></br><br></br>
+            <div className="container text-start">
+                <div class="row align-items-start text-start">
+                    <div class="col">
+                        <p className="h5">Ingredients</p><br></br>
+                        {ingredientsList.map((input, index) => ( 
+                            <div className="row" key={index}>
+                                <div className="list-group-item d-flex justify-content-between align-items-start">
+                                    <div>{input.amount} {input.units} {input.ingredient}</div>
+                                </div><br></br><br></br>
+                            </div>
+                        ))}
                     </div>
-                </ul>
-            ))}
-            <p className="h5 text-start">Directions</p><br></br>
-            {directionsList.map((directions, index) => (
-                <ul key={index}>   
-                    <div className="row">
-                        <div class="list-group-item d-flex justify-content-between align-items-start fw-bold">Step {index + 1}</div><br></br>
-                        {directions.direction}
-                    </div><br></br><br></br>   
-                </ul>
-            ))}
+                    <div className="col">
+                        <p className="h5">Directions</p><br></br>
+                        {directionsList.map((directions, index) => (
+                            <div className="row" key={index}>   
+                                <div className="list-group-item d-flex justify-content-between align-items-start fw-bold">Step {index + 1}</div><br></br>
+                                {directions.direction}
+                                <div><br></br></div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
         </div>
 
     );
