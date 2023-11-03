2435ca5b347a1a673fb060516f59aba6a9bb4cbc
Brent Hoover <bah4@txstate.edu>
2023-11-03 17:02:46 -0500

    add theme switcher to sidebar

--- src/widgets/Navbar.js
+++ src/widgets/Navbar.js
@@ -1,5 +1,4 @@
 import DisplayToggle from "../components/Utils/DisplayMode";
-import ThemeSwitcher from "../components/Utils/ThemeSwitcher";
 import Sidebar from "../widgets/Sidebar";
 import Login from "../Login"
 
@@ -11,11 +10,8 @@ function Navbar() {
                     <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <a className="navbar-brand fw-bolder px-2" href="/home"> RECIPE GENERATOR</a>
-                </div>
-                <div> 
                     <Sidebar/>
-                    <ThemeSwitcher />
+                    <a className="navbar-brand fw-bolder px-2" href="/home"> RECIPE GENERATOR</a>
                 </div>
                 <div classname="d-flex">
                     <div className="row justify-content-end align-items-center">
@@ -23,23 +19,22 @@ function Navbar() {
                             <DisplayToggle/>
                         </div>
                         <div className="col-6">
-                        {/* <form className="d-flex" role="search">
-                            <input className="form-control me-2" type="search" placeholder="ex: Lasagna, pasta.." aria-label="Search"/>
-                            <button className="btn btn-outline-success" type="submit">Search</button>
-                        </form> */}
+                            {/* <form className="d-flex" role="search">
+                                <input className="form-control me-2" type="search" placeholder="ex: Lasagna, pasta.." aria-label="Search"/>
+                                <button className="btn btn-outline-success" type="submit">Search</button>
+                            </form> */}
                             <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
                             <img class="avatar avatar-24 bg-light rounded-circle text-white p-1"
                                 src="https://raw.githubusercontent.com/twbs/icons/main/icons/person-fill.svg"></img>
                             </button>
                             <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
-                            <div class="offcanvas-header">
-                                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                                <div class="offcanvas-header">
+                                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                                </div>
+                                <div class="offcanvas-body">
+                                    <Login />
+                                </div>
                             </div>
-                            <div class="offcanvas-body">
-                                <Login />
-                            </div>
-                            </div>
-
                         </div>
                     </div>
                 </div>
--- src/widgets/Sidebar.js
+++ src/widgets/Sidebar.js
@@ -1,3 +1,4 @@
+import ThemeSwitcher from "../components/Utils/ThemeSwitcher";
 
 function Sidebar() {
     return (
@@ -11,36 +12,40 @@ function Sidebar() {
             </div>
             <div className="offcanvas-body">
                 <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-                <li><hr className="dropdown-divider/"/></li>
-                <li className="nav-item">
-                    <a className="nav-link active" href="/home">Home</a>
-                </li>
-                <li><hr className="dropdown-divider/"/></li>
-                <li className="nav-item">
-                    <a className="nav-link active" href="/create-recipe">Create Recipe</a>
-                </li>
-                <li><hr className="dropdown-divider/"/></li>
-                <li className="nav-item">
-                    <a className="nav-link active" href="/in-stock">In Stock</a>
-                </li>
-                <li><hr className="dropdown-divider/"/></li>
-                <li className="nav-item">
-                    <a className="nav-link active" href="/input-portal">Input Ingredients Portal</a>
-                </li>
-                <li><hr className="dropdown-divider/"/></li>
-                <li className="nav-item dropdown">
-                    <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    User Menu
-                    </a>
-                    <ul className="dropdown-menu">
-                    <li><a className="dropdown-item" href="/login-page">Log In</a></li>
                     <li><hr className="dropdown-divider/"/></li>
-                    <li><a className="dropdown-item" href="/profile-page">Profile</a></li>
-                    </ul>
-                </li>
+                    <li className="nav-item">
+                        <a className="nav-link active" href="/home">Home</a>
+                    </li>
+                    <li><hr className="dropdown-divider/"/></li>
+                    <li className="nav-item">
+                        <a className="nav-link active" href="/create-recipe">Create Recipe</a>
+                    </li>
+                    <li><hr className="dropdown-divider/"/></li>
+                    <li className="nav-item">
+                        <a className="nav-link active" href="/in-stock">In Stock</a>
+                    </li>
+                    <li><hr className="dropdown-divider/"/></li>
+                    <li className="nav-item">
+                        <a className="nav-link active" href="/input-portal">Input Ingredients Portal</a>
+                    </li>
+                    <li><hr className="dropdown-divider/"/></li>
+                    <li className="nav-item dropdown">
+                        <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        User Menu
+                        </a>
+                        <ul className="dropdown-menu">
+                        <li><a className="dropdown-item" href="/login-page">Log In</a></li>
+                        <li><hr className="dropdown-divider/"/></li>
+                        <li><a className="dropdown-item" href="/profile-page">Profile</a></li>
+                        </ul>
+                    </li>
                 </ul>
             </div>
+            <row>
+                <ThemeSwitcher/>
+            </row><br></br>
         </div>
+        
     )
 
 
