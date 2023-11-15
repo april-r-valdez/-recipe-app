36dc495656a8695de0358358511f3a3fa3427cac
Brent Hoover <bah4@txstate.edu>
2023-11-15 01:40:57 -0600

    add folder for footer function

--- src/widgets/Footer/Footer.js
+++ src/widgets/Footer/Footer.js
@@ -5,16 +5,20 @@ function Footer() {
     <div className="container-xl">
         <footer className="bg-body-tertiary py-3" data-bs-theme="light">
             <div className="row row-cols-1 row-cols-2 row-cols-3">
-                <div className="col-1">
-                    <img className="icon-img" src="restaurant.png" alt="" style={{ maxWidth: '30px', maxHeight: '30px' }}></img>
+                <div className="col-3">
+                    <div className="row">
+                        <div className="col-1">
+                            <img className="icon-img ms-md-3" src="restaurant.png" alt="" style={{ maxWidth: '30px', maxHeight: '30px' }}></img>
+                        </div>
+                        <div className="col-2">
+                            <Link className="navbar-brand text-md-start fw-bolder ms-md-4 nav-link active" to="/home">RECIPE GENERATOR</Link>
+                        </div>
+                    </div>
                 </div>
-                <div className="col-2">
-                    <Link className="navbar-brand text-md-start fw-bolder nav-link active" to="/home">RECIPE GENERATOR</Link>
-                </div>
-                <div class="col-4">
+                <div className="col-3">
                     <p className="navbar-brand fw-bolder text-md-center ms-md-4">&copy; 2023 KLINGONS. All rights reserved.</p>
                 </div>
-                <div class="col-5 d-flex justify-content-end">
+                <div className="col-6 d-flex justify-content-end">
                     <Link className="navbar-brand fw-bolder mx-4 nav-link active" to="/">About</Link>
                     <span className="text-muted">|</span>
                     <Link className="navbar-brand fw-bolder mx-4 nav-link active" to="/home">Contact Us</Link>
