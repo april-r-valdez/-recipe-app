976c8ffe385be67895e39932990ded67549309dc
Brent Hoover <bah4@txstate.edu>
2023-11-15 01:24:28 -0600

    add image icon to footer

--- src/widgets/Footer.js
+++ src/widgets/Footer.js
@@ -4,24 +4,25 @@ function Footer() {
     return (
     <div className="container-xl">
         <footer className="bg-body-tertiary py-3" data-bs-theme="light">
-            <div className="row row-cols-1 row-cols-2">
-            <div class="col-2">
-                {/* <img className="icon-img" alt="../public/restaurant.png"/> */}
-                <Link className="navbar-brand text-md-start ms-md-4 fw-bolder nav-link active" to="/home">RECIPE GENERATOR</Link>
-            </div>
-            <div class="col-5">
-                {/* <img className="icon-img" alt="../public/restaurant.png"/> */}
-                <p className="navbar-brand fw-bolder text-md-center ms-md-4">&copy; 2023 KLINGONS. All rights reserved.</p>
-            </div>
-            <div class="col-5 d-flex justify-content-end">
-                <Link className="navbar-brand fw-bolder mx-4 nav-link active" to="/">About</Link>
-                <span className="text-muted">|</span>
-                <Link className="navbar-brand fw-bolder mx-4 nav-link active" to="/home">Contact Us</Link>
-                <span className="text-muted">|</span>
-                <Link className="navbar-brand fw-bolder mx-4 nav-link active" to="/home">Our Team</Link>
-                <span className="text-muted">|</span>
-                <Link className="navbar-brand fw-bolder mx-4 nav-link active" to="/home">Tips & Tutorials</Link>
-            </div>
+            <div className="row row-cols-1 row-cols-2 row-cols-3">
+                <div className="col-1">
+                    <img className="icon-img" src="restaurant.png" alt="" style={{ maxWidth: '30px', maxHeight: '30px' }}></img>
+                </div>
+                <div className="col-2">
+                    <Link className="navbar-brand text-md-start fw-bolder nav-link active" to="/home">RECIPE GENERATOR</Link>
+                </div>
+                <div class="col-4">
+                    <p className="navbar-brand fw-bolder text-md-center ms-md-4">&copy; 2023 KLINGONS. All rights reserved.</p>
+                </div>
+                <div class="col-5 d-flex justify-content-end">
+                    <Link className="navbar-brand fw-bolder mx-4 nav-link active" to="/">About</Link>
+                    <span className="text-muted">|</span>
+                    <Link className="navbar-brand fw-bolder mx-4 nav-link active" to="/home">Contact Us</Link>
+                    <span className="text-muted">|</span>
+                    <Link className="navbar-brand fw-bolder mx-4 nav-link active" to="/home">Our Team</Link>
+                    <span className="text-muted">|</span>
+                    <Link className="navbar-brand fw-bolder mx-4 nav-link active" to="/home">Tips & Tutorials</Link>
+                </div>
             </div>
         </footer>
     </div>
