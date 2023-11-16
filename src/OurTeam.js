d793b8215f758d89ed0d5887542e3ef283030f43
Brent Hoover <bah4@txstate.edu>
2023-11-15 22:37:58 -0600

    design about us page

--- src/OurTeam.js
+++ src/OurTeam.js
@@ -7,51 +7,71 @@ function OurTeam() {
             <h1 className="display-5">Our Team</h1><br></br><br></br>
             <div className="row">
                 <div className="col-4">
-                    <div class="card ms-md-4">
+                    <div class="card text-bg-secondary ms-md-4">
+                        <br></br>
+                        <img src="https://mdbcdn.b-cdn.net/img/new/avatars/8.webp" class="rounded-circle mb-3 mx-auto" style={{width: '150px' }} alt="Avatar" />
                         <div class="card-body">
-                            <h5 class="card-title">Mandil Pradhan</h5>
-                            <h6 class="card-subtitle mb-2 text-body-secondary">Full-Stack Software Developer</h6>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> 
+                            <h5 class="card-title mb-3" style={{ color: 'white' }}><strong>Mandil Pradhan</strong></h5>
+                            <h6 class="card-subtitle mb-4 text-body-secondary">Full-Stack Software Developer</h6>
+                            <p class="card-text" style={{ color: 'white' }}>Some quick example text to build on the card title and make up the bulk of the card's content.</p> 
                         </div>
+                        <br></br>
                     </div>
                 </div>
                 <div className="col-4">
-                    <div class="card ms-md-4">
+                    <div class="card text-bg-secondary ms-md-4">
+                        <br></br>
+                        <img src="https://mdbcdn.b-cdn.net/img/new/avatars/8.webp" class="rounded-circle mb-3 mx-auto" style={{width: '150px' }} alt="Avatar" />
                         <div class="card-body">
-                            <h5 class="card-title">Jeffrey Hsu</h5>
-                            <h6 class="card-subtitle mb-2 text-body-secondary">Full-Stack Software Developer</h6>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> 
+                            <h5 class="card-title mb-3" style={{ color: 'white' }}><strong>Jeffrey Hsu</strong></h5>
+                            <h6 class="card-subtitle mb-4 text-body-secondary">Full-Stack Software Developer</h6>
+                            <p class="card-text" style={{ color: 'white' }}>
+                                Jeff is proficient in React.js, HTML, and CSS. 
+                                He enjoys working closely with cross-functional teams, including designers 
+                                and back-end developers, to transform ideas into functional and visually appealing digital solutions.
+                                His focus is on creating synergy between design and functionality.
+                            </p> 
                         </div>
+                        <br></br>
                     </div>
                 </div>
                 <div className="col-4">
-                    <div class="card ms-md-4">
+                    <div class="card text-bg-secondary ms-md-4">
+                        <br></br>
+                        <img src="https://mdbcdn.b-cdn.net/img/new/avatars/8.webp" class="rounded-circle mb-3 mx-auto" style={{width: '150px' }} alt="Avatar" />
                         <div class="card-body">
-                            <h5 class="card-title">Viet Vu</h5>
-                            <h6 class="card-subtitle mb-2 text-body-secondary">Full-Stack Software Developer</h6>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> 
+                            <h5 class="card-title mb-3" style={{ color: 'white' }}><strong>Viet Vu</strong></h5>
+                            <h6 class="card-subtitle mb-4 text-body-secondary">Full-Stack Software Developer</h6>
+                            <p class="card-text" style={{ color: 'white' }}>Some quick example text to build on the card title and make up the bulk of the card's content.</p> 
                         </div>
+                        <br></br>
                     </div>
                 </div>
             </div>
             <br></br><br></br>
             <div className="row">
                 <div className="col-4 mx-auto">
-                    <div class="card ms-md-4">
+                    <div class="card text-bg-secondary ms-md-4 text-center">
+                        <br></br>
+                        <img src="https://mdbcdn.b-cdn.net/img/new/avatars/8.webp" class="rounded-circle mb-3 mx-auto" style={{width: '150px' }} alt="Avatar" />
                         <div class="card-body">
-                            <h5 class="card-title">April Valdez</h5>
-                            <h6 class="card-subtitle mb-2 text-body-secondary">Front-End Software Developer</h6>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> 
+                            <h5 class="card-title mb-3" style={{ color: 'white' }}><strong>April Valdez</strong></h5>
+                            <h6 class="card-subtitle mb-4 text-body-secondary">Front-End Software Developer</h6>
+                            <p class="card-text" style={{ color: 'white' }}>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         </div>
+                        <br></br>
                     </div>
                 </div>
                 <div className="col-4 mx-auto">
-                    <div class="card ms-md-4">
+                    <div class="card text-bg-secondary ms-md-4">
+                        <br></br>
+                        <img src="https://mdbcdn.b-cdn.net/img/new/avatars/8.webp" class="rounded-circle mb-3 mx-auto" style={{width: '150px' }} alt="Avatar" />
                         <div class="card-body">
-                            <h5 class="card-title">Brent Hoover</h5>
-                            <h6 class="card-subtitle mb-2 text-body-secondary">Front-End Software Developer</h6>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> 
+                            <h5 class="card-title mb-3" style={{ color: 'white' }}><strong>Brent Hoover</strong></h5>
+                            <h6 class="card-subtitle mb-4 text-body-secondary">Front-End Software Developer</h6>
+                            <p class="card-text" style={{ color: 'white' }}>Some quick example text to build on the card title and make up the bulk of the card's content.</p> 
                         </div>
+                        <br></br>
                     </div>
                 </div>
             </div>
