4c131255f8efbce4421b3dea13295fa4517c4d23
Brent Hoover <bah4@txstate.edu>
2023-11-19 20:03:52 -0600

    modify create recipe display

--- src/components/Pantry/Pantry.js
+++ src/components/Pantry/Pantry.js
@@ -4,8 +4,8 @@ const Pantry = ({recipeImg, recipeInfo, ingredientsList, directionsList, handleD
         <div className="pantry">
             { recipeImg && (
                 <div className="container">
-                    <div>
-                        <img src={URL.createObjectURL(recipeImg)} alt="" style={{ width: "75%", height: "100%", objectFit: "cover" }}/>
+                    <div style={{ width: "auto", height: "300px", overflow: "hidden" }}>
+                        <img src={URL.createObjectURL(recipeImg)} alt="" style={{ width: "100%", height: "100%", objectFit: "cover" }}/>
                     </div><br></br>
                 </div>
             )}
@@ -36,65 +36,68 @@ const Pantry = ({recipeImg, recipeInfo, ingredientsList, directionsList, handleD
             )}
             <div className="container text-start">
                 <div class="row align-items-start text-start">
-                    <div class="col">
-                        <p className="h5 text-center">Ingredients</p><br></br>
-                        <div className="card">
-                            <div className="row g-1">
-                                <div className="col-md">
-                                    <div className="card">
-                                        <div className="card-body">
-                                            <div className="list-group list-group-flush">
-                                                {ingredientsList.map((input, index) => (
-                                                    <li key={index} className="list-group-item d-flex justify-content-between align-items-start">
-                                                        {input.amount} {input.units} {input.ingredient}
-                                                        <button type="button" className="close btn btn-sm" aria-label="Close" onClick={(e) => handleDelete(e, index, "ingredient")}>
-                                                        <span aria-hidden="true" className="text-dark">&times;</span>
-                                                        </button>
-                                                    </li>
-                                                ))}
+                    {ingredientsList && ingredientsList.length > 0 && (
+                        <div class="col-6">
+                            <p className="h5 text-center">Ingredients</p><br></br>
+                            <div className="card">
+                                <div className="row g-1">
+                                    <div className="col-md">
+                                        <div className="card">
+                                            <div className="card-body">
+                                                <div className="list-group list-group-flush">
+                                                    {ingredientsList.map((input, index) => (
+                                                        <li key={index} className="list-group-item d-flex justify-content-between align-items-start">
+                                                            {input.amount} {input.units} {input.ingredient}
+                                                            <button type="button" className="close btn btn-sm" aria-label="Close" onClick={(e) => handleDelete(e, index, "ingredient")}>
+                                                            <span aria-hidden="true" className="text-dark">&times;</span>
+                                                            </button>
+                                                        </li>
+                                                    ))}
+                                                </div>
+                                                <div className="d-grid gap-2 col-2 mx-auto" role="group" aria-label="Basic example">
+                                                    {/* <button type="button" className="btn btn-secondary" onClick={resetIngredientList}>RESET</button>                                    */}
+                                                </div>                            
                                             </div>
-                                            <div className="d-grid gap-2 col-2 mx-auto" role="group" aria-label="Basic example">
-                                                {/* <button type="button" className="btn btn-secondary" onClick={resetIngredientList}>RESET</button>                                    */}
-                                            </div>                            
                                         </div>
-                                    </div>
-                                </div> 
+                                    </div> 
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="col">
-                        <p className="h5 text-center">Directions</p><br></br>
-                        <div className="card">
-                            <div className="row g-1">
-                                <div className="col-md">
-                                    <div className="card">
-                                        <div className="card-body">
-                                            <div className="list-group list-group-flush">
-                                                {directionsList.map((directions, index) => (
-                                                    <li key={index} className="list-group-item d-flex justify-content-between align-items-start">
-                                                        <div>
-                                                        <strong>Step {index + 1}</strong><br />
-                                                        {directions.direction}
-                                                        </div>
-                                                        <button type="button" className="close btn btn-sm" aria-label="Close" onClick={(e) => handleDelete(e, index, "direction")}>
-                                                        <span aria-hidden="true" className="text-dark">&times;</span>
-                                                        </button>
-                                                    </li>
-                                                ))}
+                    )}
+                    {directionsList && directionsList.length > 0 && (
+                        <div className="col-6">
+                            <p className="h5 text-center">Directions</p><br></br>
+                            <div className="card">
+                                <div className="row g-1">
+                                    <div className="col-md">
+                                        <div className="card">
+                                            <div className="card-body">
+                                                <div className="list-group list-group-flush">
+                                                    {directionsList.map((directions, index) => (
+                                                        <li key={index} className="list-group-item d-flex justify-content-between align-items-start">
+                                                            <div>
+                                                            <strong>Step {index + 1}</strong><br />
+                                                            {directions.direction}
+                                                            </div>
+                                                            <button type="button" className="close btn btn-sm" aria-label="Close" onClick={(e) => handleDelete(e, index, "direction")}>
+                                                            <span aria-hidden="true" className="text-dark">&times;</span>
+                                                            </button>
+                                                        </li>
+                                                    ))}
+                                                </div>
+                                                <div className="d-grid gap-2 col-2 mx-auto" role="group" aria-label="Basic example">
+                                                    {/* <button type="button" className="btn btn-secondary" onClick={resetIngredientList}>RESET</button>                                    */}
+                                                </div>                            
                                             </div>
-                                            <div className="d-grid gap-2 col-2 mx-auto" role="group" aria-label="Basic example">
-                                                {/* <button type="button" className="btn btn-secondary" onClick={resetIngredientList}>RESET</button>                                    */}
-                                            </div>                            
                                         </div>
-                                    </div>
-                                </div> 
+                                    </div> 
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    )}
                 </div>
             </div>
         </div>
-
     );
 }
 
