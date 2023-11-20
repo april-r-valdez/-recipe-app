0f6baed2ed5ca906e8eb786f06589d0bc9d9912e
Mandil Pradhan <jns176@txstate.edu>
2023-11-20 01:44:07 -0600

    Update to Navbar and Footer. Replaced hardcoding of icons

--- src/widgets/Navbar.js
+++ src/widgets/Navbar.js
@@ -2,6 +2,8 @@ import DisplayToggle from "../components/Utils/DisplayMode";
 import Sidebar from "../widgets/Sidebar";
 import Login from "../Login"
 import { Link } from "react-router-dom";
+import { IoIosSearch } from "react-icons/io";
+import { FaCircleUser } from "react-icons/fa6";
 
 
 function Navbar() {
@@ -21,15 +23,11 @@ function Navbar() {
                             <form className="d-flex me-3 col-10" role="search">
                                 <input className="form-control me-2" type="search" placeholder="Search for recipe (ex: lasagna,...)" aria-label="Search"/>
                                 <button className="btn btn-primary" type="submit">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
-                                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-                                    </svg>
+                                <IoIosSearch style={{ fontSize: '20px'}}/>
                                 </button>
                             </form>
                             <button className="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
-                            <img className="avatar avatar-24 bg-light rounded-circle text-white p-1"
-                                src="https://raw.githubusercontent.com/twbs/icons/main/icons/person-fill.svg"
-                                alt="avatar"></img>
+                            <FaCircleUser style={{ fontSize: '20px'}} />
                             </button>
                             <div className="offcanvas offcanvas-end" tabIndex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
                                 <div className="offcanvas-header">
