c972ee3f81f9b241ac89faf6ce0dba9561679b6d
Jeff H <cuk8@txstate.edu>
2023-11-26 16:49:52 -0600

    displays shopping list from firestore
    the doc format for shopping list and pantry differs slightly -> will have to do a pass accordingly to schema

--- src/components/Utils/ShoppingList/ShoppingList.js
+++ src/components/Utils/ShoppingList/ShoppingList.js
@@ -1,19 +1,29 @@
 import React, { useState, useEffect } from "react";
-import { firestore } from "../../../firebase";
+import { db } from "../../../firebase";
+import { collection, doc, getDoc } from "firebase/firestore";
 
 const ShoppingList = ({ userID }) => {
   const [shoppingList, setShoppingList] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
-      try {
-        const shoppingListRef = firestore
-          .collection("ShoppingList")
-          .doc(userID);
-        const snapshot = await shoppingListRef.get();
+      if (!userID) {
+        alert("You're not signed in!");
+        console.log("ERR: Not signed in.");
+        return;
+      }
 
-        if (snapshot.exists) {
-          setShoppingList(snapshot.data().items || []);
+      try {
+        const shoppingListRef = collection(db, "ShoppingLists");
+
+        // look for existing user shopping list
+        const userDocRef = doc(shoppingListRef, userID);
+        console.log("userID: ", userID);
+        const snapshot = await getDoc(userDocRef);
+
+        if (snapshot.exists()) {
+          console.log(snapshot.data());
+          setShoppingList(snapshot.data());
         }
       } catch (error) {
         console.error("Error fetching shopping list:", error);
@@ -26,24 +36,22 @@ const ShoppingList = ({ userID }) => {
   const renderShoppingListItem = (item, index) => {
     return (
       <tr key={index}>
-        <td>{item.amount}</td>
-        <td>{item.name}</td>
+        <td>{item}</td>
+        <td>{shoppingList[item]}</td>
       </tr>
     );
   };
 
   return (
-    <div className="container-sm text-center">
+    <div className="container-sm text-center mt-5">
       <p className="h3">Shopping List</p>
 
       <table className="table">
-        <thead>
-          <tr>
-            <th scope="col">Amount</th>
-            <th scope="col">Name</th>
-          </tr>
-        </thead>
-        <tbody>{shoppingList.map(renderShoppingListItem)}</tbody>
+        <tbody>
+          {Object.keys(shoppingList).map((item, index) =>
+            renderShoppingListItem(item, index)
+          )}
+        </tbody>
       </table>
     </div>
   );
