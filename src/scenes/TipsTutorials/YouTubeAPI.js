6ef71d3c8bec826fec0b3cf6b868ebbcc5fbda57
Mandil Pradhan <jns176@txstate.edu>
2023-11-27 21:46:53 -0600

    Auto stash before checking out "origin/sprint3"

--- src/scenes/TipsTutorials/YouTubeAPI.js
+++ src/scenes/TipsTutorials/YouTubeAPI.js
@@ -1,7 +1,7 @@
 
 import axios from 'axios';
 
-const API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
+const API_KEY = "AIzaSyDY2A072lOuDB7n1J-cIQeI89Vx5f5dbtk";
 
 const fetchVideos = async (query) => {
   try {
